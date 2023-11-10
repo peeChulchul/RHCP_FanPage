6c5a61f0d82c5a47f3d3f081d9ddbeded0593f4b
peeChulchul <eyeless356@naver.com>
2023-11-10 10:07:27 +0900

    기본설정

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyles from "styles/globalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>
+);
 // <React.StrictMode>
 // </React.StrictMode>
 
---
+++ src/styles/globalStyles.jsx
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    :root {
+    /* colors */
+    --color-bg: var(--color-pink);
+    --color-primary: var(--color-yellow);
+    --color-primary-alt:var(--color-blue);
+    --color-accent: var(--color-red);
+    --color-text: var( --color-white);
+
+    --color-pink : #FAD1D7;
+    --color-yellow:#FFEC88;
+    --color-blue:#36499B;
+    --color-red:#ED1C25;
+    --color-dark-pink:#F2D7E4;
+    --color-black: #000000de;
+    --color-white: #ffffff;
+
+    /* spacing */
+    --spacing: 4px;
+
+    --font-xl: 3rem;
+    --font-lg: 2rem;
+    --font-md: 1.25rem;
+    --font-sm:0.875rem
+    }
+    /* 공통 */
+    * {
+    box-sizing: border-box;
+    color: var(--color-text);
+    }
+`;
+
+export default GlobalStyles;
